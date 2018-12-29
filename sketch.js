7080039c67af2bbd03486d51ff4ccf764fd7659c
PassiSchoppi <40070838+PassiSchoppi@users.noreply.github.com>
2018-12-29 13:16:59 +0100

    Fertig
    es funktioniert alles und das Projekt ist jetzt vorbei
    Design sieht auch schoen aus ;)
    	modified:   sketch.js

--- sketch.js
+++ sketch.js
@@ -1,30 +1,40 @@
-var rad = 100;
-var ellCenX = 250;
-var ellCenY = 250;
-var rotat = 100;
+var radius = 200;
+var KreisX = 250;
+var KreisY = 250;
+
+var anzalP = 100;
+var multiplikator = 1;
 
 function setup() {
   frameRate(30);
 	createCanvas(500, 500);
 }
 
-function getCoordinates(deg){
-  deg*=TWO_PI;
-  var coordinates = [(rad*Math.cos(deg))+ellCenX, (rad*Math.sin(deg))+ellCenY];
-  return coordinates;
+function getkoordinaten(kreisAnteil){
+  kreisAnteil*=TWO_PI;
+  var koordinaten = [(radius*Math.cos(kreisAnteil))+KreisX, (radius*Math.sin(kreisAnteil))+KreisY];
+  return koordinaten;
 }
 
 function draw() {
   background(100);
   //Kreis
   noFill();
-  stroke(0);
-  ellipse(ellCenX, ellCenY, rad*2, rad*2);
+  stroke(255);
+  ellipse(KreisX, KreisY, radius*2, radius*2);
   
-  //echter Punkt
-  for(i=0; i<rotat; i++){
+  //Berechnung
+  for(punktID=0; punktID<anzalP; punktID++){
+    multiplikator+=0.0002;
+    //erster Punkt
+    var ersterP = getkoordinaten((1/anzalP)*punktID);
     noStroke();
-    fill(255, 0, 0);
-    ellipse(getCoordinates((1/rotat)*i)[0], getCoordinates((1/rotat)*i)[1], 5, 5);
+    fill(255);
+    ellipse(ersterP[0], ersterP[1], 3, 3);
+    //zweiter Punkt
+    var zweiterP = getkoordinaten((1/anzalP)*punktID*multiplikator);
+    //Linine
+    stroke(255);
+    line(ersterP[0], ersterP[1], zweiterP[0], zweiterP[1]);
   }
 }
