7c4913c4165621f768607343a2b128a07f546fb3
PassiSchoppi <40070838+PassiSchoppi@users.noreply.github.com>
2019-03-24 13:52:50 +0100

    english code
    added picture to readme
    	modified:   LICENSE
    	modified:   README.md
    	new file:   TTV.jpg
    	modified:   sketch.js

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
-# TTD
-Times Table Diagram
+# Times-Tables-Visualization
 
 writen in javascript
-with p5*js
+with the p5*js library
+inspired by @shiffman
+[This video ](https://www.youtube.com/watch?v=qhbuKbxJsk8)explains the mathematics behind this phenomenon.
+
+![here should be a pic](TTV.jpg)
--- sketch.js
+++ sketch.js
@@ -1,50 +1,43 @@
 var radius = 300;
-var KreisX = 1366/2;
-var KreisY = 650/2;
+var circleX = 1366/2;
+var circleY = 650/2;
 
-var anzalP = 500;
-var multiplikator = 0;
+var numberOfPoints = 500;
+var times = 0;
 
 function setup() {
-  frameRate(1)
+  frameRate(30)
   createCanvas(windowWidth, windowHeight);
 }
 
-function getkoordinaten(kreisAnteil){
-  kreisAnteil*=TWO_PI;
-  var koordinaten = [(radius*Math.cos(kreisAnteil))+KreisX, (radius*Math.sin(kreisAnteil))+KreisY];
-  return koordinaten;
-}
-
 function draw() {
-  anzalP = 500;
+  numberOfPoints = 500;
   background(100);
   strokeWeight(1);
+  times+=0.01;
 
-  multiplikator++;
-
-  //Kreis
+  // circle
   noFill();
   stroke(255);
-  ellipse(KreisX, KreisY, radius*2, radius*2);
+  ellipse(circleX, circleY, radius*2, radius*2);
 
-  //Text
-  textSize(30);
-  fill(255);
-  text(multiplikator, 100, 100, 100, 100);
-
-  //Berechnung
-  for(punktID=0; punktID<anzalP; punktID++){
-    //erster Punkt
-    var ersterP = getkoordinaten((1/anzalP)*punktID);
-    ellipse(ersterP[0], ersterP[1], 3, 3);
-    //zweiter Punkt
-    var zweiterP = getkoordinaten((1/anzalP)*punktID*multiplikator);
-    //Linine
-    line(ersterP[0], ersterP[1], zweiterP[0], zweiterP[1]);
+  for(punktID=0; punktID<numberOfPoints; punktID++){
+    // first point
+    var firstP = getPointCoordinates((1/numberOfPoints)*punktID);
+    ellipse(firstP[0], firstP[1], 3, 3);
+    // second point
+    var secondP = getPointCoordinates((1/numberOfPoints)*punktID*times);
+    // line
+    line(firstP[0], firstP[1], secondP[0], secondP[1]);
   }
 }
 
+function getPointCoordinates(proportionOfCircle){
+  proportionOfCircle*=TWO_PI;
+  var coordinates = [(radius*Math.cos(proportionOfCircle))+circleX, (radius*Math.sin(proportionOfCircle))+circleY];
+  return coordinates;
+}
+
 function mousePressed() {
-  multiplikator=0;
+  times=0;
 }
